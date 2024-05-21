c206be54e56db9082368eb7336afe7c6c3019c83
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2024-05-21 00:43:59 +0000

    modify default url, port

--- apps/web/app/trpc.ts
+++ apps/web/app/trpc.ts
@@ -2,9 +2,9 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import { AppRouter } from "@server/trpc/trpc.router";
 
 export const trpc = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
-    }),
-  ],
+	links: [
+		httpBatchLink({
+			url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER ?? "http://localhost:4000"}/trpc`,
+		}),
+	],
 });
